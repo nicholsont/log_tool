535fcaa4d0817fd497948a173e4e44dbc7707798
Tijan Nicholson <nicholson.tijan@gmail.com>
2017-11-22 00:22:48 -0800

    editted to meet pep8 standards

--- log_tool.py
+++ log_tool.py
@@ -1,56 +1,67 @@
-from log_tooldb import get_articles,get_authors,get_access_logs
+from log_tooldb import get_articles, get_authors, get_access_logs
 from datetime import datetime
 import os
 
-#Write request log
+# Creates analysis log
+
+
 def prep_log():
     print('Processing Request log...')
-    #Retrive data from DB
+    # Retrive data from DB
     articles = get_articles()
     authors = get_authors()
     access_logs = get_access_logs()
 
-    #Process data for log file output
+    # Process data for log file output
     articles_data = process_articles(articles)
     authors_data = process_authors(authors)
     access_logs_data = process_access_logs(access_logs)
 
-    #Create log file
+    # Create log file
     path = os.path.dirname(os.path.abspath(__file__))
     output_file = open(path + '/request_log.txt', 'w')
     output_file.write(articles_data + authors_data + access_logs_data)
     output_file.close()
     print('Log created.')
 
-#Process logs for top 3 viewed articles
-#Params: articles
+# Process logs for top 3 viewed articles
+# Params: articles
+
+
 def process_articles(articles):
     log_data = ''
     log_data += '***Top 3 most viewed articles***\n'
     for article in articles:
-        log_data += '\t"%(title)s" -- %(count)d views\n' % {'title': article[0].title(),'count': article[1]}
+        replace = {'title': article[0].title(), 'count': article[1]}
+        log_data += '\t"%(title)s" -- %(count)d views\n' % replace
     return log_data
 
-#Process logs for authors ordered by most article views
-#Params: authors
+# Process logs for authors ordered by most article views
+# Params: authors
+
+
 def process_authors(authors):
     log_data = ''
     log_data += '\n***Authors by most article views***\n'
     for author in authors:
-        log_data += '\t%(author)s -- %(count)d views\n' % {'author': author[0],'count': author[1]}
+        replace = {'author': author[0], 'count': author[1]}
+        log_data += '\t%(author)s -- %(count)d views\n' % replace
     return log_data
 
-#Process logs for access errors exceeding 1%
-#Params: access_logs
+# Process logs for access errors exceeding 1%
+# Params: access_logs
+
+
 def process_access_logs(access_logs):
     log_data = ''
     log_data += '\n***Request errors that exceed 1%***\n'
     for log in access_logs:
-        #Calculate the error percentage('404 status'/'200 status')
+        # Calculate the error percentage('404 status'/'200 status')
         percent = round(float(log[2])/float(log[1]) * 100, 2)
         if percent >= 1:
+            replace = {'log': log_date.strftime('%B %d, %Y'), 'count': percent}
             log_date = datetime.strptime(log[0], '%Y-%m-%d')
-            log_data += '\t%(log)s -- %(count).2f%% errors\n' % {'log': log_date.strftime('%B %d, %Y'),'count': percent}
+            log_data += '\t%(log)s -- %(count).2f%% errors\n' % replace
     return log_data
 
 prep_log()
--- log_tooldb.py
+++ log_tooldb.py
@@ -1,15 +1,17 @@
 import psycopg2
 
-#Set database name
+# Set database name
 DBNAME = "news"
 
-#Retrieve articles ordered by most views
+# Retrieve articles ordered by most views
+
+
 def get_articles():
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
     query = "select art.title, count(*) as article_views "
-    query += "from public.authors as aut, public.articles as art, public.log as l "
-    query += "where aut.id = art.author and l.path like '/article/%'||art.slug "
+    query += "from public.authors as aut, public.articles as art, public.log as l "  # noqa
+    query += "where aut.id = art.author and l.path like '/article/%'||art.slug "  # noqa
     query += "group by art.title "
     query += "order by article_views desc "
     query += "limit 3;"
@@ -17,30 +19,34 @@ def get_articles():
     return c.fetchall()
     db.close()
 
-#Retrieve authors ordered by most views
+# Retrieve authors ordered by most views
+
+
 def get_authors():
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
     query = "select aut.name, count(*) as article_views "
-    query += "from public.authors as aut, public.articles as art, public.log as l "
-    query += "where aut.id = art.author and l.path like '/article/%'||art.slug "
+    query += "from public.authors as aut, public.articles as art, public.log as l "  # noqa
+    query += "where aut.id = art.author and l.path like '/article/%'||art.slug "  # noqa
     query += "group by aut.name, l.status "
     query += "order by article_views desc;"
     c.execute(query)
     return c.fetchall()
     db.close()
 
-#Retrieve log data grouped by status, ordered by most views
+# Retrieve log data grouped by status, ordered by most views
+
+
 def get_access_logs():
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
     query = "SELECT OK.date, OK.num, ERROR.num FROM "
-    query += "(SELECT to_char(date(time), 'YYYY-MM-DD') as date, count(*) as num "
+    query += "(SELECT to_char(date(time), 'YYYY-MM-DD') as date, count(*) as num "  # noqa
     query += "FROM public.log "
     query += "WHERE status = '200 OK' "
     query += "GROUP BY status, date "
     query += "ORDER BY date ASC) AS OK, "
-    query += "(SELECT to_char(date(time), 'YYYY-MM-DD') as date, count(*) as num "
+    query += "(SELECT to_char(date(time), 'YYYY-MM-DD') as date, count(*) as num "  # noqa
     query += "FROM public.log "
     query += "WHERE status = '404 NOT FOUND' "
     query += "GROUP BY status, date "
